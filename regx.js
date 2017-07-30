f8508da5ef7ea82df214314642e9b121ef4263a9
Vijendra Kumar S K <skvijay007@gmail.com>
2017-07-30 15:21:20 +0530

    Revised source file
    Revised source file

--- regx.js
+++ regx.js
@@ -2,7 +2,7 @@
 //regx.number({check:val,startingdecimal:true, integer:{length:3},decimals:{length:2,optional:true},sign:{show:true,optional:true},currency:{sign:'$',optional:true},brackets:{show:true,optional:true}}); regx.number(val);
 //regx.mac({check:val,delimiter:[]}); regx.mac(value)
 //regx.email({check:val,identifier:{has:[],include:{elements:[],optional:true},exclude:{elements:[],optional:true}},domain:{name:'gmail'},tld:{type:['com','co']}}); regx.email(value)
-//regx.replace({check:'',having:{custom:''}});
+
 
 
 
